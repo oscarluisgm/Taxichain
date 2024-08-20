9ea58a0b908a40a32957cbd1647f1929671059b5
Oscar Guerra <oscarluisgm2@gmail.com>
2024-08-20 13:48:26 -0400

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,19 @@ function App() {
   return (
     <>
       <ToastContainer/>
-      <div className="nav-bar">
+      <div className="header">
+        <div className="home">
         <img src={pictures.logo} height="80px" />
         <button className="wallet-button"> <i className="bi bi-wallet wallet-icon"></i> Connect Wallet</button>
+        </div>
+      <div className="nav-bar d-flex d-md-none">
+          <Link className="sidebar-link" to={"/marketplace"}>Marketplace</Link>
+          <Link className="sidebar-link">City</Link>
+          <Link className="sidebar-link">Mining</Link>
+          <Link className="sidebar-link">Parking</Link>
+          <Link className="sidebar-link">Profile</Link>
+          <Link className="sidebar-link">Crafting</Link>
+      </div>
       </div>
       <div className="layout-taxichain">
         <div className="sidebar d-none d-md-flex">
