e26a53113b4e1252807a9bfd96efaaa24c90496e
unknown <oscarluisgm2@gmail.com>
2024-09-01 11:52:29 -0400

    details

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Sidebar = () => {
+
+  const[borderStyle, setBorderStyle]=useState("")
+
+  const bStyle={
+    normal:{},
+    clicked:{border:'3px solid white', backgroundColor:'rgb(255, 166, 0)'}
+  }
+  
+
+
   return (
     <>
       <div className="sidebar d-none d-md-flex">
-        <Link className="sidebar-link" to={"/marketplace"}>
+        <Link className="sidebar-link" to={"/marketplace"} onClick={()=>setBorderStyle("marketplace")} style={borderStyle==="marketplace" ? bStyle.clicked:bStyle.normal}>
           Marketplace
         </Link>
-        <Link className="sidebar-link">City</Link>
-        <Link className="sidebar-link">Mining</Link>
-        <Link className="sidebar-link" to={"/parking"}>Parking</Link>
-        <Link className="sidebar-link">Profile</Link>
-        <Link className="sidebar-link">Crafting</Link>
+        <Link className="sidebar-link" /* onClick={()=>setBorderStyle("city")} style={borderStyle==="city" ? bStyle.clicked:bStyle.normal} */>City</Link>
+        <Link className="sidebar-link" /* onClick={()=>setBorderStyle("mining")} style={borderStyle==="mining" ? bStyle.clicked:bStyle.normal} */>Mining</Link>
+        <Link className="sidebar-link" to={"/parking"} onClick={()=>setBorderStyle("parking")} style={borderStyle==="parking" ? bStyle.clicked:bStyle.normal}>Parking</Link>
+        <Link className="sidebar-link" /* onClick={()=>setBorderStyle("profile")} style={borderStyle==="profile" ? bStyle.clicked:bStyle.normal} */>Profile</Link>
+        <Link className="sidebar-link" /* onClick={()=>setBorderStyle("crafting")} style={borderStyle==="crafting" ? bStyle.clicked:bStyle.normal} */>Crafting</Link>
       </div>
     </>
   );
