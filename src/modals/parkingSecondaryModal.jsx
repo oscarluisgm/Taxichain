84f9f6d0a73eb65a55c9244d5873e86b20de030b
unknown <oscarluisgm2@gmail.com>
2024-09-04 23:35:37 -0400

    some details ok

--- src/modals/parkingSecondaryModal.jsx
+++ src/modals/parkingSecondaryModal.jsx
@@ -4,7 +4,7 @@ export const SecondaryParking=({buttonAction, handleParking, handleCancel})=>{
     return <>
         <div className="secondary-modal-bg">
             <div className="secondary-modal-container card">
-                <div>You are about to {buttonAction} this car. Do you confirm this action?</div>
+                <div>You are about to {buttonAction} this car. Do you confirm this?</div>
                 <div className="secondary-modal-buttons">
                     <button className="confirm bg-success" onClick={handleParking} >OK</button>
                     <button className="cancel bg-danger" onClick={handleCancel}>Cancel</button>
